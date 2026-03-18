b778d51b7adeb282463b2c36cede396519dd80e4
skyhealthmedia <contact@skyhealthmedia.com>
2026-03-18 15:30:47 -0600

    Update clients.ts

--- src/clients.ts
+++ src/clients.ts
@@ -1,24 +1,30 @@
-const clients = {
+type ClientConfig = {
+  name: string;
+  domain: string;
+  ga4_property_id: string;
+};
+
+const clients: Record<string, ClientConfig> = {
   skyhealth: {
     name: 'SkyHealth Media',
     domain: 'skyhealthmedia.com',
-    ga4_property_id: '501364602'
+    ga4_property_id: '501364602',
   },
   kernplacepediatrics: {
     name: 'Kern Place Pediatrics',
     domain: 'kernplacepediatrics.com',
-    ga4_property_id: '528543159'
+    ga4_property_id: '528543159',
   },
   pediatricgi: {
     name: 'Pediatric GI of El Paso',
     domain: 'elppedsgi.com',
-    ga4_property_id: '528517254'
+    ga4_property_id: '528517254',
   },
   vipeds: {
     name: 'VIPeds Pediatric Hospital Medicine',
     domain: 'vipeds.org',
-    ga4_property_id: '529057672'
-  }
+    ga4_property_id: '529057672',
+  },
 };
 
 export default clients;
